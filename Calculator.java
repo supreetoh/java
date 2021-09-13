060038dba645cce88730a7e81b7adc36f24a1275
supreetoh <supreetoh@gmail.com>
2021-09-13 09:28:41 +0530

    Second One

---
+++ Calculator.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+public class Calculator {
+    
+    public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter First Number");
+		int a = sc.nextInt();
+		
+		System.out.println("Enter Second Number");
+		int b = sc.nextInt();
+		
+		System.out.println("Enter The Operation");
+		sc.nextLine();
+		char operation = sc.nextLine().charAt(0);
+		
+		int result = 0;
+		
+		switch(operation) {
+		case '+':
+			result = a + b;
+			break;
+		case '-':
+			result = a - b;
+			break;
+		case '*':
+			result = a * b;
+			break;
+		case '/':
+			result = a / b;
+			break;	
+		default:
+		     System.out.println("invalid operation");
+	
+		}
+		
+		System.out.println("The Result Is " + result);
+	}
+
+}
+
+
